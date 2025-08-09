1532a3a40d3095ecca6e6d26b8e955bc588c41f1
Aladdin Ardıç <alaaddinardic46a@gmail.com>
2025-08-09 23:33:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import fs from 'fs';
 
-const supabaseUrl = 'https://nvniedwdxijfjgpjwqxg.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52bmllZHdkeGlqZmpncGp3cXhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1NDUxMzUsImV4cCI6MjA2NzEyMTEzNX0.7oU7UflADHrcaduW0m6ssBtRWRHW5alaT9wemeR7UO8'
+const supabaseUrl = 'url'
+const supabaseKey = 'key'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function fetchData() {
